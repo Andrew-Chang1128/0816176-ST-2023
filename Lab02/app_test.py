b9d4f46a2af27f64389cd9a014138a68bdbfbd64
david-chenyu <58728741+david-chenyu@users.noreply.github.com>
2023-03-09 00:18:43 +0800

    Create app_test.py

---
+++ Lab02/app_test.py
@@ -0,0 +1,34 @@
+import unittest
+from unittest.mock import patch
+from unittest.mock import Mock
+import app
+
+class ApplicationTest(unittest.TestCase):
+
+    @patch('app.Application.get_names')
+    def setUp(self, mockGetNames):
+        # stub
+        mockGetNames.return_value = (["William", "Oliver", "Henry", "Liam"], ["William", "Oliver", "Henry"])
+        self.App = app.Application()
+        print(f'people stubbed {self.App.people}, selected stubbed: {self.App.selected}')
+
+        pass
+
+    @patch('app.MailSystem.send')
+    @patch('app.Application.get_random_person', side_effect = ["William", "Oliver", "Henry", "Liam"])
+    def test_app(self, mockGetRandomPerson, mockSend):
+        # spy
+        def mockSend_side_effect(*args, **kwargs):
+            print(args[1])
+        mockSend.side_effect = mockSend_side_effect
+        # mock
+        nextPerson = self.App.select_next_person()
+        self.assertEqual(nextPerson, 'Liam')
+        print(f'{nextPerson} selected!')
+        self.App.notify_selected()
+        self.assertEqual(mockSend.call_count,4)
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
