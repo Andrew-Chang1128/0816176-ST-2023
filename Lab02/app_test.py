fa37c77a2f3cc86c6a9420033fd2e224700d10b9
david-chenyu <58728741+david-chenyu@users.noreply.github.com>
2023-03-09 23:51:12 +0800

    Update app_test.py

--- Lab02/app_test.py
+++ Lab02/app_test.py
@@ -11,23 +11,23 @@ class ApplicationTest(unittest.TestCase):
         mockGetNames.return_value = (["William", "Oliver", "Henry", "Liam"], ["William", "Oliver", "Henry"])
         self.App = app.Application()
         print(f'people stubbed {self.App.people}, selected stubbed: {self.App.selected}')
-        pass
 
+        pass
     @patch('app.MailSystem.write')
     @patch('app.MailSystem.send')
     @patch('app.Application.get_random_person', side_effect = ["William", "Oliver", "Henry", "Liam"])
     def test_app(self, mockGetRandomPerson, mockSend, mockWrite):
         # Mock Send function by creating a new func and assign it to side effect of Mocksend
-        def mockSend_side_effect(*args, **kwargs):
-            print(args[1])
-        mockSend.side_effect = mockSend_side_effect
+        def mockWrite_side_effect(*args, **kwargs):
+            print(f'Congrats, {args[0]}!')
+        mockWrite.side_effect = mockWrite_side_effect
         # mock get_random_person func which will be called by select_next_person
         nextPerson = self.App.select_next_person()
         self.assertEqual(nextPerson, 'Liam')
         print(f'{nextPerson} selected!')
         self.App.notify_selected()
-        self.assertEqual(mockSend.call_count,4)
         self.assertEqual(mockWrite.call_count,4)
+        self.assertEqual(mockSend.call_count,4)
         pass
 
 
